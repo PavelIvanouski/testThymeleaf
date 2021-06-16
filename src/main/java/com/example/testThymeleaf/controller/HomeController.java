1bff1e6102a40f8c59ae89659b08a652d439ec1f
Pavel Ivanouski <pavel_85_67@mail.ru>
2021-06-16 15:03:18 +0200

    initial commit

---
+++ src/main/java/com/example/testThymeleaf/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.example.testThymeleaf.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class HomeController {
+    @GetMapping("/home")
+    public String home(@RequestParam(required = false) String login, Model model) {
+        System.out.println("login is: " + login);
+        model.addAttribute("login",login);
+        return "home";
+    }
+}
